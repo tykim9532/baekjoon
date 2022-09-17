654652a35f81a9be80681d46f69833578463b1c7
tykim <tykim@zconverter.com>
2022-09-17 14:48:33 +0900

    2439

---
+++ 2439.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void cinout() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main() {
+    cinout();
+    int a; cin >> a;
+    
+    return 0;
+}
